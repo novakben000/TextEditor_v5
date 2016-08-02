39a3ac28864f10731367330f03f6461cb998e909
Ben <Ben@BENSPC>
2016-08-02 10:32:54 -0700

    Add project files.

---
+++ TextEditor/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Windows;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TextEditor
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        //Clears the text box when the "New" button is clicked
+        void NewButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.textBox.Text = "";
+        }
+
+        //Sets the font size for the text editor
+        void SetFontSizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            double size = Double.Parse(this.fontSizeBox.Text);
+            this.textBox.FontSize = size;
+        }
+
+        //Opens dialog to open file
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileSavePicker savePicker = new FileSavePicker();
+            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = "New Document";
+            StorageFile file = await savePicker.PickSaveFileAsync();
+
+            if (file != null)
+            {
+                // Prevent updates to the remote version of the file until
+                // we finish making changes and call CompleteUpdatesAsync.
+                CachedFileManager.DeferUpdates(file);
+                // write to file
+                await FileIO.WriteTextAsync(file, textBox.Text);
+                // Let Windows know that we're finished changing the file so
+                // the other app can update the remote version of the file.
+                // Completing updates may require Windows to ask for user input.
+                Windows.Storage.Provider.FileUpdateStatus status = await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
+                if (status == Windows.Storage.Provider.FileUpdateStatus.Complete)
+                {
+                    this.outputTextBlock.Text = "File " + file.Name + " was saved.";
+                }
+                else
+                {
+                    this.outputTextBlock.Text = "File " + file.Name + " couldn't be saved.";
+                }
+            }
+            else
+            {
+                this.outputTextBlock.Text = "Operation cancelled.";
+            }
+        }
+
+        private async void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            openPicker.FileTypeFilter.Add(".txt");
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            textBox.Text = await FileIO.ReadTextAsync(file);
+
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                outputTextBlock.Text = "Picked file: " + file.Name;
+            }
+            else
+            {
+                outputTextBlock.Text = "Operation cancelled.";
+            }
+        }
+
+        //Opens dialog to save file
+    }
+}
